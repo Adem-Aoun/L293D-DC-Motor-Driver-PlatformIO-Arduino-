13dd9a32f4d1927f2ae3fc76b0fa7a42ae3b10d6
Adem aoun <a-adem@outlook.fr>
2026-04-27 14:48:35 +0100

    improved driver

--- src/l293d_driver.c
+++ src/l293d_driver.c
@@ -7,29 +7,38 @@ static inline int map_speed(float speed){
 }
 
 void setup_motor(Motor *motor){
+    if(!motor) return;
     pinMode(motor->INP1,OUTPUT);
     pinMode(motor->INP2,OUTPUT);
     pinMode(motor->ENABLE12,OUTPUT);
 
+    digitalWrite(motor->INP1,0);
+    digitalWrite(motor->INP2,0);
+    analogWrite(motor->ENABLE12,0);
+
 }
 
 void cc_motor(Motor *motor,float speed){
+    if(!motor) return;
     digitalWrite(motor->INP1,1);
     digitalWrite(motor->INP2,0);
     analogWrite(motor->ENABLE12,map_speed(speed));
 }
 
 void ccw_motor(Motor *motor,float speed){
+    if(!motor) return;
     digitalWrite(motor->INP1,0);
     digitalWrite(motor->INP2,1);
     analogWrite(motor->ENABLE12,map_speed(speed));
 }
 
 void stop_motor(Motor *motor){
-    digitalWrite(motor->ENABLE12,0);
+    if(!motor) return;
+    analogWrite(motor->ENABLE12,0);
 }
 
 void brake_motor(Motor *motor){
+    if(!motor) return;
     digitalWrite(motor->INP1,1);
     digitalWrite(motor->INP2,1);
     analogWrite(motor->ENABLE12,255);
